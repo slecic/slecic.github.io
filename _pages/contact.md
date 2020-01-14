7c3f807da20eebc8d026cb1681232dc4b2732a85
Sonja Lecic <sonjalecic@Sonjas-MacBook-Pro.local>
2020-01-14 20:42:53 +0100

    2020

---
+++ _pages/contact.md
@@ -0,0 +1,14 @@
+---
+permalink: /contact/
+title: "Contact"
+excerpt: "Contact me"
+author_profile: true
+---
+Contact information is below, including emails, and other places where I live online.
+
+* E-mail: thomas.m.wolfe [at] gmail.com
+* Twitter: [t_wolfe](https://twitter.com/MupdnW)
+* Google Scholar: [author:thomas m wolfe](https://scholar.google.com/citations?user=gEFelJQAAAAJ&hl=en)
+* LinkedIn: [thomas-wolfe](https://www.linkedin.com/in/thomas-wolfe-b4b829179/)
+* University of Vienna: [Research Group Plant Ecological Genomics](https://plantgenomics.univie.ac.at/)
+* University of Veterinary Medicine Vienna: [Vienna Graduate School of Population Genetics](https://www.popgen-vienna.at/people/students/)
---
+++ talk_map/org-locations.js
@@ -0,0 +1,22 @@
+var addressPoints = [
+  [
+    "Fort Worth, Texas | Botany | <a href='http://twolfe.github.io/talks/2018-07-25-botany'>Botany Conference</a>",
+    32.758255,
+    -97.326859
+  ],
+  [
+    "Cambridge, UK | PopGroup | <a href='http://twolfe.github.io/talks/2018-01-04-popgroup'>PopGroup Conference</a>",
+    52.205337,
+    0.121817
+  ],
+  [
+    "Rovinj, Croatia | International Conference on Polyploidy, Hybridization and Biodiversity | <a href='http://twolfe.github.io//talks/2016-05-11-icp'>Polyploidy Conference</a>",
+    45.081166,
+    13.638707
+  ],
+  [
+    "Vienna, Austria | 48th Annual Meeting of the Ecological Society of Germany, Austria and Switzerland | <a href='http://twolfe.github.io//talks/2018-09-10-ecologicalSoc'>Ecology Conference</a>",
+    48.231554,
+    16.357586
+  ]
+];
