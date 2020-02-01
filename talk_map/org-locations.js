a694fdc78372b67326e10e86eece0af1ae74b94a
slecic <slecic8@gmail.com>
2020-02-01 21:40:04 +0100

    feb2020

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
   [
-    "Fort Worth, Texas | Botany | <a href='http://twolfe.github.io/talks/2019-01-04-popgroup'>PopGroup 52</a>",
+    "Oxford, UK | PopGroup52 | <a href='http://slecic.github.io/talks/2019-01-04-popgroup'>PopGroup 52</a>",
     51.754991,
     -1.224663
   ]
