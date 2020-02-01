f0df433e320f1799b6c8da0a543126b2287dc95c
slecic <slecic8@gmail.com>
2020-02-01 21:48:27 +0100

    1.2.2020

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
   [
-    "Oxford, UK | <a href='http://www.genetics.org.uk/events/pop-group/'>PopGroup 52</a>",
+    "Oxford, UK | PopGroup52 | <a href='http://www.genetics.org.uk/events/pop-group/'>PopGroup 52</a>",
     51.754991,
     -1.224663
   ]
